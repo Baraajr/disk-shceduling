40aa85d8f69a5157bd3d13b8d33a2e4a7767efcb
Ahmed baraa <ahmedbaraa009@gmail.com>
2025-04-28 19:35:33 +0300

    read inputs from user

---
+++ main.py
@@ -0,0 +1,7 @@
+queue_size = input("Enter the size of the queue: ")
+number_of_requests = int(input("Enter the number of requests: "))
+initial_head_posizison = input("Enter the initial head position: ")
+number_of_ylinders = input('Enter the number of cylinders: ')
+algorithm_to_be_used = input('Enter the algorithm to be used(F:FCFD | S:Scan| C:CScan): ')
+step_cylinder_time = input('Enter the time taken to move from one cylinder to another: ')
+
