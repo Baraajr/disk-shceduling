80b32c64a5afe1e02bb232d386f5eb09a551cf87
Nariman Emam <141886157+NarimanEmam@users.noreply.github.com>
2025-04-30 21:55:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,16 @@ number_of_ylinders = input('Enter the number of cylinders: ')
 algorithm_to_be_used = input('Enter the algorithm to be used(F:FCFD | S:Scan| C:CScan): ')
 step_cylinder_time = input('Enter the time taken to move from one cylinder to another: ')
 
+
+def fcfs(requests, head, step_time):
+    order = [head]
+    total_movement = 0
+
+    for req in requests:
+        movement = abs(req - head)
+        total_movement += movement
+        head = req
+        order.append(req)
+
+    total_seek_time = total_movement * step_time
+    return order, total_movement, total_seek_time
