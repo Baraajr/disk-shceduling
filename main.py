e23fec54578c3b24fea7a8ae6f781c3356ea3121
Ahmed baraa <ahmedbaraa009@gmail.com>
2025-05-02 17:50:35 +0300

    updated inputs, added cscan

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-queue_size = input("Enter the size of the queue: ")
+queue_size = int(input("Enter the size of the queue: "))
 number_of_requests = int(input("Enter the number of requests: "))
-initial_head_posizison = input("Enter the initial head position: ")
-number_of_ylinders = input('Enter the number of cylinders: ')
-algorithm_to_be_used = input('Enter the algorithm to be used(F:FCFD | S:Scan| C:CScan): ')
-step_cylinder_time = input('Enter the time taken to move from one cylinder to another: ')
-
+initial_head_position = int(input("Enter the initial head position: "))
+number_of_cylinders = int(input('Enter the number of cylinders: '))
+algorithm_to_be_used = input('Enter the algorithm to be used (F: FCFS | S: Scan | C: C-Scan): ').upper()
+step_cylinder_time = float(input('Enter the time taken to move from one cylinder to another: '))
 
 def fcfs(requests, head, step_time):
     order = [head]
@@ -62,6 +61,38 @@ def scan_algorithm(requests, head, direction, disk_size, step_time):
                 total_head_movement += abs(head - r)
                 head = r
                 seek_sequence.append(r)
+
+    total_seek_time = total_head_movement * step_time
+
+    return seek_sequence, total_head_movement, total_seek_time
+
+
+def cscan(requests, head, disk_size, step_time):
+    requests.sort()
+    seek_sequence = []
+    total_head_movement = 0
+
+    # Separate requests into those greater and less than head
+    left = [req for req in requests if req < head]
+    right = [req for req in requests if req >= head]
+
+    # Service requests to the right of head
+    for req in right:
+        total_head_movement += abs(head - req)
+        head = req
+        seek_sequence.append(req)
+
+    # Move head to the end of the disk (simulate circular jump to beginning)
+    if right:
+        total_head_movement += abs(head - (disk_size - 1))
+        head = 0
+        total_head_movement += abs((disk_size - 1) - head)
+
+    # Service requests to the left
+    for req in left:
+        total_head_movement += abs(head - req)
+        head = req
+        seek_sequence.append(req)
 
     total_seek_time = total_head_movement * step_time
 
